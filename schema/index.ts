9fd544c8c3d10adbc789313e9f71e048fd127900
Рома <timeflieswhenyouhf@internet.ru>
2026-08-20 22:04:11 +0300

    soft cards

--- schema/index.ts
+++ schema/index.ts
@@ -1,13 +1,16 @@
 import { defineRelations } from "drizzle-orm";
 import { cards } from "./cards";
 import { tasks } from "./tasks";
+import { softCards } from "./soft-cards";
 
 export const schema = {
   cards,
   tasks,
+  softCards,
 };
 
 export const relations = defineRelations(schema, (r) => ({
   cards: {},
+  softCards: {},
   tasks: {},
 }));
---
+++ schema/soft-cards.ts
@@ -0,0 +1,31 @@
+import {
+  boolean,
+  jsonb,
+  pgTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { ulid } from "ulid";
+
+export type CardPayload = {
+  telegram: string | null;
+  description: string | null;
+};
+
+export const softCards = pgTable("soft_cards", {
+  id: varchar()
+    .primaryKey()
+    .notNull()
+    .$defaultFn(() => ulid()),
+  website: varchar().notNull(),
+  createdAt: timestamp({ withTimezone: true, precision: 0 })
+    .notNull()
+    .defaultNow(),
+  title: varchar().notNull(),
+  label: varchar().notNull(),
+  isClosed: boolean().notNull().default(false),
+  payload: jsonb()
+    .$type<CardPayload>()
+    .notNull()
+    .default({ description: null, telegram: null }),
+});
